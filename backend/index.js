4a4cc8f9521e19d51105f15a4dbb884a249c4e5a
James Mahfouz <jamesmahfouz0@gmail.com>
2023-05-24 11:00:52 +0300

    created the auth route

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
 const express = require('express')
+const cluster = require("cluster");
+const OS = require("os")
+const cors = require('cors');
+require("dotenv").config();
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const authRouter = require('./routes/auth.routes')
+app.use('/auth', authRouter)
