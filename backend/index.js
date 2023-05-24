dfd82bbfb72390b2e623d82d53c2654d75c69a3f
James Mahfouz <jamesmahfouz0@gmail.com>
2023-05-24 11:38:46 +0300

    added the database configuration

--- backend/index.js
+++ backend/index.js
@@ -11,3 +11,16 @@ app.use(express.urlencoded({ extended: true }))
 
 const authRouter = require('./routes/auth.routes')
 app.use('/auth', authRouter)
+
+if (cluster.isMaster) {
+    const numCpus = OS.cpus().length;
+    for (let i = 0; i < numCpus; i++) {
+        cluster.fork();
+    }
+} else {
+    app.listen(process.env.PORT, (err) => {
+        if (err) console.error(err)
+        console.log(`Worker ${process.pid} is running on port `, process.env.PORT);
+        require("./configs/db.config")
+    });
+}
